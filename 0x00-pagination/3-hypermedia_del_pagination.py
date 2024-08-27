14291adce15d96451fc5c9a6fca9c16844e83084
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-08-27 13:04:39 +0200

    i18n

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -10,6 +10,7 @@ from typing import Dict, List, Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
     """
+
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
